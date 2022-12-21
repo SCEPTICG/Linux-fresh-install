3802b4d3a7b3833c556329c72776f20f5ed5a9f3
SCEPTICG <scepticgames001@gmail.com>
2022-12-21 20:02:07 +0100

    Update conf.sh

--- conf.sh
+++ conf.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-. instalacion.sh
-
-sudo apt install fzy
+. Ubuntu.sh
 
 PS3='Please enter your choice: '
 options=(Ubuntu "Debian based distribution" Fedora "Arch or Arch based distrobution" Quit)
@@ -28,28 +26,3 @@ do
         *) echo "invalid option $REPLY";;
     esac
 done
-
-do
-    options=(opt1 "option 2" another "option here")
-    echo "Please choose an option:"
-    printf "%s\n" "${options[@]}" | fzy
-    case $options in
-        "Ubuntu")
-            echo "you chose Ubuntu"
-            ;;
-        "Debian based distribution")
-            echo "you chose Debian based distribution"
-            ;;
-        "Fedora")
-            echo "you chose Fedora"
-            ;;
-        "Arch or Arch based distrobution")
-            echo "you chose Arch or Arch based distrobution"
-            ;;
-        "Quit")
-            break
-            ;;
-        *) echo "invalid option $REPLY";;
-    esac
-done
-
