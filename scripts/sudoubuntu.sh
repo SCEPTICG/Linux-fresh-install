ee56dac1e584eec960e3fe961ea1949175f1c2b3
SCEPTICG <scepticgames001@gmail.com>
2022-12-21 21:35:52 +0100

    Update sudoubuntu.sh

--- scripts/sudoubuntu.sh
+++ scripts/sudoubuntu.sh
@@ -40,7 +40,7 @@ ln -s -f /home/scepticg/.zshrc /root/.zshrc
 chsh -s $(which zsh)
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
-cp p10k.zsh ~/.p10k.zsh
+cp ../configp10k.zsh ~/.p10k.zsh
 
 echo "====================================================="
 echo "                   Installing FZF                    "
