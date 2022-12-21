1a1f3c15162c0742152762c76699f3f13e95358f
SCEPTICG <scepticgames001@gmail.com>
2022-12-21 21:29:31 +0100

    Update ubuntu.sh

--- scripts/ubuntu.sh
+++ scripts/ubuntu.sh
@@ -14,10 +14,21 @@ echo "          Automated Ubuntu Config Installer          "
 echo "====================================================="
 echo ""
 
+echo "====================================================="
+echo "                   Updating ubuntu                   "
+echo "====================================================="
+echo ""
+sleep 2
 #ACTUALIZAMOS UBUNTU
 sudo apt update 
 sudo apt upgrade -y
 
+
+echo "====================================================="
+echo " Uninstalling Snap firefox and installig deb version "
+echo "====================================================="
+echo ""
+sleep 2
 #INSTALAR FIREFOX COMO .DEB
 sudo snap remove firefox
 #Añadimos el ppa de Mozilla Team
@@ -31,12 +42,23 @@ Pin-Priority: 1001
 #Nos aseguramos que las actualizaciones se hagan automaticamente
 echo 'Unattended-Upgrade::Allowed-Origins:: "LP-PPA-mozillateam:${distro_codename}";' | sudo tee /etc/apt/apt.conf.d/51unattended-upgrades-firefox
 
+
+echo "====================================================="
+echo "                Add PPA Repositories                 "
+echo "====================================================="
+echo ""
+sleep 2
 #AÑADIMOS LOS PPA QUE NECESTIAMOS
 #Vscode
 sudo apt install software-properties-common apt-transport-https wget -y
 wget -O- https://packages.microsoft.com/keys/microsoft.asc | sudo gpg --dearmor | sudo tee /usr/share/keyrings/vscode.gpg
 echo deb [arch=amd64 signed-by=/usr/share/keyrings/vscode.gpg] https://packages.microsoft.com/repos/vscode stable main | sudo tee /etc/apt/sources.list.d/vscode.list
 
+echo "====================================================="
+echo "                 Installing packages                 "
+echo "====================================================="
+echo ""
+sleep 2
 #INSTALAMOS TODOS LOS PAQUETES
 sudo apt update
 sudo apt install firefox code nala virtualbox git kitty neofetch ubuntu-restricted-extras -y
@@ -47,6 +69,12 @@ wget https://github.com/shiftkey/desktop/releases/download/release-3.1.1-linux1/
 sudo dpkg -i GitHubDesktop-linux-3.1.1-linux1.deb
 rm GitHubDesktop-linux-3.1.1-linux1.deb
 
+
+echo "====================================================="
+echo "                Installing Nerd Fonts                "
+echo "====================================================="
+echo ""
+sleep 2
 #DESCARGAMOS E INSTALAMOS NERDFONTS
 git clone https://github.com/ryanoasis/nerd-fonts.git
 cd nerd-fonts
@@ -54,11 +82,22 @@ cd nerd-fonts
 cd ..
 rm -rf nerd-fonts
 
+echo "====================================================="
+echo "                 Installing Astrovim                 "
+echo "====================================================="
+echo ""
+sleep 2
 #INSTALAMOS ASTROVIM
 mv ~/.local/share/nvim/site ~/.local/share/nvim/site.bak
 git clone https://github.com/AstroNvim/AstroNvim ~/.config/nvim
 nvim +PackerSync
 
+
+echo "====================================================="
+echo "  Installing and configuring ZSH with powerlevel10k  "
+echo "====================================================="
+echo ""
+sleep 2
 #INSTALAMOS ZSH Y CONFIGURAMOS POWERLEVEL10K
 sudo apt install zsh -y
 chsh -s $(which zsh)
@@ -78,6 +117,12 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
 cp ../config/p10k.zsh ~/.p10k.zsh
 
+
+echo "====================================================="
+echo "                   Installing FZF                    "
+echo "====================================================="
+echo ""
+sleep 2
 #INSTALAMOS FZF
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
