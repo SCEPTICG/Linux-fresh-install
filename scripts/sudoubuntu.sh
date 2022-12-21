fed849d7788e49b3d6c6c27ec1785720c390e57d
SCEPTICG <scepticgames001@gmail.com>
2022-12-21 21:33:26 +0100

    Update sudoubuntu.sh

--- scripts/sudoubuntu.sh
+++ scripts/sudoubuntu.sh
@@ -2,10 +2,29 @@
 
 # MIRAMOS QUE EL SCRIPT SE EJECUTA COMO ROOT
 if [[ $EUID -ne 0 ]]; then
-  echo "Tienes que ser root para ejecutar este script, porfavor ejecuta sudo ./sudoconf.sh" 2>&1
+  echo "Tienes que ser root para ejecutar este script, porfavor ejecuta sudo sudoubuntu.sh" 2>&1
   exit 1
 fi
 
+echo "========================================================================================"
+echo ""
+echo "░██████╗██╗░░░██╗██████╗░░█████╗░ ██╗░░░██╗██████╗░██╗░░░██╗███╗░░██╗████████╗██╗░░░██╗"
+echo "██╔════╝██║░░░██║██╔══██╗██╔══██╗ ██║░░░██║██╔══██╗██║░░░██║████╗░██║╚══██╔══╝██║░░░██║"
+echo "╚█████╗░██║░░░██║██║░░██║██║░░██║ ██║░░░██║██████╦╝██║░░░██║██╔██╗██║░░░██║░░░██║░░░██║"
+echo "░╚═══██╗██║░░░██║██║░░██║██║░░██║ ██║░░░██║██╔══██╗██║░░░██║██║╚████║░░░██║░░░██║░░░██║"
+echo "██████╔╝╚██████╔╝██████╔╝╚█████╔╝ ╚██████╔╝██████╦╝╚██████╔╝██║░╚███║░░░██║░░░╚██████╔╝"
+echo "╚═════╝░░╚═════╝░╚═════╝░░╚════╝░ ░╚═════╝░╚═════╝░░╚═════╝░╚═╝░░╚══╝░░░╚═╝░░░░╚═════╝░"
+echo ""
+echo "========================================================================================"
+echo "                        Automated Ubuntu Config Installer for sudo                      "
+echo "========================================================================================"
+echo ""
+
+echo "====================================================="
+echo "  Installing and configuring ZSH with powerlevel10k  "
+echo "====================================================="
+echo ""
+sleep 2
 #HACEMOS UN LINK SYMBOLICO DE LA CONFIGURACIÓN DE ZSHRC Y CAMBIAMOS LA SHELL PARA ROOT
 ln -s -f /home/scepticg/.zshrc /root/.zshrc
 chsh -s $(which zsh)
