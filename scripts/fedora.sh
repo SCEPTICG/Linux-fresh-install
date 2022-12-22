75548092eae4b376516ca1f273e59cbd28347c00
SCEPTICG <scepticgames001@gmail.com>
2022-12-22 20:58:18 +0100

    Update fedora.sh

--- scripts/fedora.sh
+++ scripts/fedora.sh
@@ -20,4 +20,31 @@ sleep 2
 
 sudo cp conf/dnf.conf /etc/dnf/dnf.conf
 
+echo"================================================"
+echo"                Updating Fedora                 "
+echo"================================================"
+sleep 2
+
+sudo dnf upgrade -y
+
+echo"================================================"
+echo"              Enabling RPM Fusion               "
+echo"================================================"
+sleep 2
+
+sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
+sudo dnf groupupdate core -y
+
+echo"================================================"
+echo"            Add Flathub Repository              "
+echo"================================================"
+sleep 2
+
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+echo"================================================"
+echo"             Install media codecs               "
+echo"================================================"
+sleep 2
+
 
