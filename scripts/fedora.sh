8b01655b9185f2accfce0aef8d99ccd70e768f6d
SCEPTICG <scepticgames001@gmail.com>
2022-12-22 21:13:42 +0100

    Update fedora.sh

--- scripts/fedora.sh
+++ scripts/fedora.sh
@@ -70,6 +70,6 @@ echo"================================================"
 echo ""
 sleep 2
 
-sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc -y
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 sudo dnf install firefox code virtualbox-7.0 git kitty neofetch -y
