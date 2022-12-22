c7d05acdde1b2d3ff19ee092de9ff0eea8e353e0
SCEPTICG <scepticgames001@gmail.com>
2022-12-22 16:31:34 +0100

    Update debubu.sh

--- scripts/debubu.sh
+++ scripts/debubu.sh
@@ -48,7 +48,7 @@ sleep 2
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 sudo install -o root -g root -m 644 microsoft.gpg /usr/share/keyrings/microsoft-archive-keyring.gpg
 sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/usr/share/keyrings/microsoft-archive-keyring.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-
+sudo apt install apt-transport-https
 
 echo "====================================================="
 echo "                 Installing packages                 "
