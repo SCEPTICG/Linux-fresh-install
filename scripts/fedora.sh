b97e409268b79d1b7f867b12a83ccda28bef37b0
SCEPTICG <scepticgames001@gmail.com>
2022-12-22 21:38:34 +0100

    Update fedora.sh

--- scripts/fedora.sh
+++ scripts/fedora.sh
@@ -1,50 +1,50 @@
 #!/bin/bash
 
-echo"================================================"
-echo""
-echo"███████╗███████╗██████╗░░█████╗░██████╗░░█████╗░"
-echo"██╔════╝██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗"
-echo"█████╗░░█████╗░░██║░░██║██║░░██║██████╔╝███████║"
-echo"██╔══╝░░██╔══╝░░██║░░██║██║░░██║██╔══██╗██╔══██║"
-echo"██║░░░░░███████╗██████╔╝╚█████╔╝██║░░██║██║░░██║"
-echo"╚═╝░░░░░╚══════╝╚═════╝░░╚════╝░╚═╝░░╚═╝╚═╝░░╚═╝"
-echo""
-echo"================================================"
-echo"       Automated Fedora Config Installer        "  
-echo"================================================"
+echo "================================================"
+echo ""
+echo "███████╗███████╗██████╗░░█████╗░██████╗░░█████╗░"
+echo "██╔════╝██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗"
+echo "█████╗░░█████╗░░██║░░██║██║░░██║██████╔╝███████║"
+echo "██╔══╝░░██╔══╝░░██║░░██║██║░░██║██╔══██╗██╔══██║"
+echo "██║░░░░░███████╗██████╔╝╚█████╔╝██║░░██║██║░░██║"
+echo "╚═╝░░░░░╚══════╝╚═════╝░░╚════╝░╚═╝░░╚═╝╚═╝░░╚═╝"
+echo ""
+echo "================================================"
+echo "       Automated Fedora Config Installer        "  
+echo "================================================"
 
-echo"================================================"
-echo"               Making DNF better                "
-echo"================================================"
+echo "================================================"
+echo "               Making DNF better                "
+echo "================================================"
 sleep 2
 
-sudo cp conf/dnf.conf /etc/dnf/dnf.conf
+sudo cp config/dnf.conf /etc/dnf/dnf.conf
 
-echo"================================================"
-echo"                Updating Fedora                 "
-echo"================================================"
+echo "================================================"
+echo "                Updating Fedora                 "
+echo "================================================"
 sleep 2
 
 sudo dnf upgrade -y
 
-echo"================================================"
-echo"              Enabling RPM Fusion               "
-echo"================================================"
+echo "================================================"
+echo "              Enabling RPM Fusion               "
+echo "================================================"
 sleep 2
 
 sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
 sudo dnf groupupdate core -y
 
-echo"================================================"
-echo"            Add Flathub Repository              "
-echo"================================================"
+echo "================================================"
+echo "            Add Flathub Repository              "
+echo "================================================"
 sleep 2
 
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
-echo"================================================"
-echo"             Install media codecs               "
-echo"================================================"
+echo "================================================"
+echo "             Install media codecs               "
+echo "================================================"
 sleep 2
 
 sudo dnf groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin -y
@@ -57,17 +57,17 @@ sudo dnf swap mesa-vdpau-drivers.i686 mesa-vdpau-drivers-freeworld.i686 -y
 sudo dnf install rpmfusion-free-release-tainted -y
 sudo dnf install libdvdcss -y
 
-echo"================================================"
-echo"      Enable maximize and minimize button       "
-echo"================================================"
+echo "================================================"
+echo "      Enable maximize and minimize button       "
+echo "================================================"
 sleep 2
 
 gsettings set org.gnome.desktop.wm.preferences button-layout 'appmenu:minimize,maximize,close'
 
-echo"================================================"
-echo"               Installing packages              "
-echo"================================================"
-echo""
+echo "================================================"
+echo "               Installing packages              "
+echo "================================================"
+echo ""
 sleep 2
 
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
@@ -76,10 +76,10 @@ sudo rpm --import https://packagecloud.io/shiftkey/desktop/gpgkey
 sudo sh -c 'echo -e "[shiftkey]\nname=GitHub Desktop\nbaseurl=https://packagecloud.io/shiftkey/desktop/el/7/\$basearch\nenabled=1\ngpgcheck=0\nrepo_gpgcheck=1\ngpgkey=https://packagecloud.io/shiftkey/desktop/gpgkey' > /etc/yum.repos.d/shiftkey-desktop.repo
 sudo dnf install firefox code VirtualBox git kitty neovim github-desktop neofetch -y
 
-echo"================================================"
-echo"              Installing Nerd Fonts             "
-echo"================================================"
-echo""
+echo "================================================"
+echo "              Installing Nerd Fonts             "
+echo "================================================"
+echo ""
 sleep 2
 git clone https://github.com/ryanoasis/nerd-fonts.git
 cd nerd-fonts
@@ -87,20 +87,20 @@ cd nerd-fonts
 cd ..
 rm -rf nerd-fonts
 
-echo"================================================"
-echo"               Installing Astrovim              "
-echo"================================================"
-echo""
+echo "================================================"
+echo "               Installing Astrovim              "
+echo "================================================"
+echo ""
 sleep 2
 
 mv ~/.local/share/nvim/site ~/.local/share/nvim/site.bak
 git clone https://github.com/AstroNvim/AstroNvim ~/.config/nvim
 nvim +PackerSync
 
-echo"================================================"
-echo"Installing and configuring ZSH with powerlevel10k"
-echo"================================================"
-echo""
+echo "================================================"
+echo "Installing and configuring ZSH with powerlevel10k"
+echo "================================================"
+echo ""
 sleep 2
 
 sudo dnf install zsh -y
@@ -121,10 +121,10 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
 cp config/p10k.zsh ~/.p10k.zsh
 
-echo"================================================"
-echo"                 Installing FZF                 "
-echo"================================================"
-echo""
+echo "================================================"
+echo "                 Installing FZF                 "
+echo "================================================"
+echo ""
 sleep 2
 
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
