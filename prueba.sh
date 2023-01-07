00dfb45854a61f74a436d78da41379f38e2006b5
SCEPTICG <scepticgames001@gmail.com>
2023-01-07 23:47:29 +0100

    Delete prueba.sh

--- prueba.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-yad --title "Linux fresh install script" --from --columns=4 --with=549 --height=190 --text="My config script for my linux customization"
---field="<b>Ubuntu</b>":fbtn "terminal -e sh './scripts/ubuntu.sh'"\
