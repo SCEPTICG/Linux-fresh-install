f4ea7dadf8d77a997659a85c63e1046b855794d0
SCEPTICG <scepticgames001@gmail.com>
2023-01-07 23:32:04 +0100

    Update prueba.sh

--- prueba.sh
+++ prueba.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-yad --title "Linux fresh install script" --from --columns=4 --with=549 --height=190 --text="My config script for my limux customization"
+yad --title "Linux fresh install script" --from --columns=4 --with=549 --height=190 --text="My config script for my linux customization"
 --field="<b>Ubutu</b>":fbnt "gnome-terminal -e sh './scripts/ubuntu.sh'"\
