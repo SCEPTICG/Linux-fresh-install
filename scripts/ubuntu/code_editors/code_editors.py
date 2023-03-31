ebe84678a43897bfa512ee68644a70bf987d6d80
SCEPTICG <scepticgames001@gmail.com>
2023-03-31 16:43:50 +0200

    Update code_editors.py

--- scripts/ubuntu/code_editors/code_editors.py
+++ scripts/ubuntu/code_editors/code_editors.py
@@ -3,14 +3,16 @@ import subprocess
 from pick import pick
 
 title = 'Which Code editor do you want to install: '
-options = ['VS Code', 'Sublime Text 4', 'Brave', 'Vim', 'NeoVim',  'Don\'t install a code editor']
+options = ['VS Code', 'Sublime Text 4', 'Pycharm', 'Vim', 'NeoVim',  'Don\'t install a code editor']
 
 option, index = pick(options, title, indicator='=>', default_index=0)
 
 match option:
     case 'VS Code':
-        subprocess.call(['sh', './scripts/ubuntu/'])
+        subprocess.call(['sh', './scripts/ubuntu/code_editors/vscode.sh'])
     case 'Sublime Text 4':
+        subprocess.call(['sh', './scripts/ubuntu/'])
+    case 'Pycharm':
         subprocess.call(['sh', './scripts/ubuntu/'])
     case 'Vim':
         subprocess.call(['sh', './scripts/ubuntu/'])
