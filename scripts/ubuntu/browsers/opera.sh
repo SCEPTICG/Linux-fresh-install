e6c2cc1208b0571f373eaeebeb907e5eb0ed0f36
SCEPTICG <114002154+SCEPTICG@users.noreply.github.com>
2023-03-31 12:29:51 +0200

    cambios

--- scripts/ubuntu/browsers/opera.sh
+++ scripts/ubuntu/browsers/opera.sh
@@ -4,4 +4,4 @@ sudo apt install software-properties-common apt-transport-https wget ca-certific
 wget -O- https://deb.opera.com/archive.key | sudo gpg --dearmor | sudo tee /usr/share/keyrings/opera.gpg
 echo deb [arch=amd64 signed-by=/usr/share/keyrings/opera.gpg] https://deb.opera.com/opera-stable/ stable non-free | sudo tee /etc/apt/sources.list.d/opera.list
 sudo apt update
-sudo apt install opera-stable
+sudo apt install opera-stable -y
