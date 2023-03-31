db4a1cddeb0b6c644009200bf8cb6ff492dd98ff
SCEPTICG <114002154+SCEPTICG@users.noreply.github.com>
2023-03-31 12:28:14 +0200

    Update vivaldi.sh

--- scripts/ubuntu/browsers/vivaldi.sh
+++ scripts/ubuntu/browsers/vivaldi.sh
@@ -4,4 +4,4 @@ sudo apt install wget gnupg2 ubuntu-keyring
 wget -O- https://repo.vivaldi.com/archive/linux_signing_key.pub | sudo gpg --dearmor | sudo tee /usr/share/keyrings/vivaldi.gpg
 echo deb [arch=amd64 signed-by=/usr/share/keyrings/vivaldi.gpg] https://repo.vivaldi.com/archive/deb/ stable main | sudo tee /etc/apt/sources.list.d/vivaldi.list
 sudo apt update
-sudo apt install vivaldi-stable
+sudo apt install vivaldi-stable -y
